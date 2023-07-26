0a5d6749e89b5dc146396b4a4be314e2bd5ab57b
Christopher Chay <2vendor@hisfootsteps.net>
2023-07-26 10:48:36 -0400

    fixed missing semicolon

--- src/Shop.py
+++ src/Shop.py
@@ -135,7 +135,7 @@ class Shop(commands.Cog):
 
                                         rarity = random.choices(rarities, rng, k=1)[0]
 
-                                        if rarity in "sr ur".split(" ")
+                                        if rarity in "sr ur".split(" "):
                                             await Database.generateCard(index=card_index, owner=user_id, rarity=rarity, evo=1)
                                         else:
                                             await Database.generateFodder(owner=user_id, index=card_index)
